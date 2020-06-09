09bebd94c7d1cefeaaa81ee7605ab062447386db
Aigerim KYDYRBEKOVA <kydyrbek_a@qwasar.io>
2020-06-09 21:05:21 +0000

    working with list

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -g3 -Wall -O3 -std=c99 -fsanitize=address
 
 SOURCES = main.c
 # HEADERS = my_tar_main.h
-OBJECTS = $(SOURCES:.c=.o) $(HEADERS)
+OBJECTS = $(SOURCES:.c=.o)
 TARGET = my_blockchain
 
 $(TARGET): $(OBJECTS)
--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 #define BID_SIZE 10;
 struct blockchain_Node {
     int nid;
-    int bid_size;
-    char** bid_array;
+    // int bid_size;
+    // char** bid_array;
     struct blockchain_Node *next;
 } blockchain;
 
 struct blockchain_Node* createNode(int nid_numb);
-void addNode(struct blockchain_Node *node, int nid);
+struct blockchain_Node* addNode(struct blockchain_Node *node, int nid);
 
 struct blockchain_Node* search_for_a_node(struct blockchain_Node *node, int nid_numb);
 void deleteNode(struct blockchain_Node *node, int nid_numb);
@@ -24,14 +24,16 @@ struct blockchain_Node* createNode(int nid_numb)
 {
     struct blockchain_Node *bc = (struct blockchain_Node *) malloc(sizeof(struct blockchain_Node));
     bc->nid = nid_numb;
-    bc->bid_size =BID_SIZE;
+    // bc->bid_size =BID_SIZE;
     bc->next = NULL;
 
     return bc;
 }
 
-void addNode(struct blockchain_Node *node, int nid_numb)
+struct blockchain_Node* addNode(struct blockchain_Node *node, int nid_numb)
 {
+    printf("Hello from addNode!\n");
+
     struct blockchain_Node* current = node;
 
     //get last node
@@ -40,13 +42,17 @@ void addNode(struct blockchain_Node *node, int nid_numb)
     }
     //create node with given nid
     struct blockchain_Node* new_Node = createNode(nid_numb);
+    printf("new_Node: %p \n", new_Node);
+    printf("new_Node->nid: %d \n", new_Node->nid);
     // add new node to the list
     if(current==NULL) {
         current = new_Node;
+        printf("current==NULL: %d \n", current->nid);
     } else {
         current->next = new_Node;
+        printf("current->next: %p \n", new_Node);
     }
-    
+    return current;
 }
 
 struct blockchain_Node* search_for_a_node(struct blockchain_Node *node, int nid_numb)
@@ -76,18 +82,22 @@ void deleteNode(struct blockchain_Node *node, int nid_numb)
 
 int main(int argc, const char* argv[])
 {
-  
-   struct blockchain_Node *first_node = NULL;
-   addNode(first_node, 12);
-   addNode(first_node, 13);
+   printf("Hello from main!\n");
+   struct blockchain_Node *first_node=NULL;
+   struct blockchain_Node *new_node1 = addNode(first_node, 12);
+   struct blockchain_Node *new_node2 = addNode(new_node1, 100);
+//   addNode(first_node, 13); 
    int i = 0; 
-   struct blockchain_Node *current = first_node;
+   struct blockchain_Node *current = new_node2;
+//    printf("Current: %p", current);
   while(current) {
-      printf("NODE[%d] : %d", i, first_node->nid);
+      printf("NODE[%d] : %d\n", i, current->nid);
       i++;
       current = current->next;
-      free(current);
   }
 //   free(first_node);
+    // while(first_node !=NULL && first_node->next != NULL) {
+
+    // }
     return 0;
 }
