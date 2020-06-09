ed606369b966747e3fd83faccda0d9348b564705
Aigerim KYDYRBEKOVA <kydyrbek_a@qwasar.io>
2020-06-09 21:47:20 +0000

    having leak problem

--- main.c
+++ main.c
@@ -12,10 +12,10 @@ struct blockchain_Node {
 } blockchain;
 
 struct blockchain_Node* createNode(int nid_numb);
-struct blockchain_Node* addNode(struct blockchain_Node *node, int nid);
+void addNode(struct blockchain_Node *node, int nid);
 
 struct blockchain_Node* search_for_a_node(struct blockchain_Node *node, int nid_numb);
-void deleteNode(struct blockchain_Node *node, int nid_numb);
+struct blockchain_Node* deleteNode(struct blockchain_Node *node, int nid_numb);
 
 int addBid(struct blockchain_Node *node, int nid_numb, char* bid_string);
 int rmBid(struct blockchain_Node *node, char* bid_string);
@@ -28,9 +28,10 @@ struct blockchain_Node* createNode(int nid_numb)
     bc->next = NULL;
 
     return bc;
+    free(bc);
 }
 
-struct blockchain_Node* addNode(struct blockchain_Node *node, int nid_numb)
+void addNode(struct blockchain_Node *node, int nid_numb)
 {
     printf("Hello from addNode!\n");
 
@@ -42,17 +43,18 @@ struct blockchain_Node* addNode(struct blockchain_Node *node, int nid_numb)
     }
     //create node with given nid
     struct blockchain_Node* new_Node = createNode(nid_numb);
-    printf("new_Node: %p \n", new_Node);
-    printf("new_Node->nid: %d \n", new_Node->nid);
+    // printf("new_Node: %p \n", new_Node);
+    // printf("new_Node->nid: %d \n", new_Node->nid);
     // add new node to the list
     if(current==NULL) {
         current = new_Node;
-        printf("current==NULL: %d \n", current->nid);
+        // printf("current==NULL: %d \n", current->nid);
     } else {
         current->next = new_Node;
-        printf("current->next: %p \n", new_Node);
+        // printf("current->next: %p \n", new_Node);
     }
-    return current;
+    // free(new_Node);
+    // return current;
 }
 
 struct blockchain_Node* search_for_a_node(struct blockchain_Node *node, int nid_numb)
@@ -68,7 +70,7 @@ struct blockchain_Node* search_for_a_node(struct blockchain_Node *node, int nid_
      return current; 
 }
 
-void deleteNode(struct blockchain_Node *node, int nid_numb)
+struct blockchain_Node* deleteNode(struct blockchain_Node *node, int nid_numb)
 {
     struct blockchain_Node* current = node;
     struct blockchain_Node* new = NULL;
@@ -78,26 +80,36 @@ void deleteNode(struct blockchain_Node *node, int nid_numb)
         }
     }
     *node=*new;
+    return new;
 }
 
 int main(int argc, const char* argv[])
 {
    printf("Hello from main!\n");
-   struct blockchain_Node *first_node=NULL;
-   struct blockchain_Node *new_node1 = addNode(first_node, 12);
-   struct blockchain_Node *new_node2 = addNode(new_node1, 100);
-//   addNode(first_node, 13); 
-   int i = 0; 
-   struct blockchain_Node *current = new_node2;
-//    printf("Current: %p", current);
-  while(current) {
-      printf("NODE[%d] : %d\n", i, current->nid);
-      i++;
-      current = current->next;
-  }
-//   free(first_node);
-    // while(first_node !=NULL && first_node->next != NULL) {
+   // the very first node with nid = 0; its like genesis node.. 
+   struct blockchain_Node *first_node=createNode(0);
 
-    // }
+    addNode(first_node, 12);
+    // free(first_node);
+    addNode(first_node, 9);
+    // free(first_node);
+   int i = 0; 
+//    struct blockchain_Node *current = new_node2;
+//    printf("Current: %p", current);
+  while(first_node) {
+      printf("NODE[%d] : %d\n", i, first_node->nid);
+      i++;
+      first_node = first_node->next;
+  }
+
+    struct blockchain_Node *tmp;
+    while(first_node != NULL)
+    {
+        tmp = first_node;
+        first_node = first_node->next;
+        free(tmp);
+    }
+    free(first_node);
+
     return 0;
 }
